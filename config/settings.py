0664a53ca2d688995f8c2510194833eb2e7095e9
derijablya <dulugov@gmail.com>
2025-04-16 16:03:08 +0500

    refactoring

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
